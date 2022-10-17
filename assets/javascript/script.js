fd7f027f891a7eb555a37c338aa2bfab34e15e63
Alex Clayton <alexander.clayton.email@gmail.com>
2022-10-16 22:42:58 -0500

    added local storage javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -48,6 +48,31 @@ function whatTime() {
 }
 whatTime();
 
+$("#0 .description").val(localStorage.getItem("0"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
+$("#6 .description").val(localStorage.getItem("6"));
+$("#7 .description").val(localStorage.getItem("7"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
+$("#21 .description").val(localStorage.getItem("21"));
+$("#22 .description").val(localStorage.getItem("22"));
+$("#23 .description").val(localStorage.getItem("23"));
+
 
 
 /* wasn't able to get the "description" div through this method...
